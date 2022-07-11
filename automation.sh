b5d642b2f10439f1fbdeaed00a25d31bfafa3cd9
Ganga-R-1996 <gangamailslot@gmail.com>
2022-07-11 09:10:52 +0530

    Added codes to check the running and enabled status of apache2

--- automation.sh
+++ automation.sh
@@ -3,3 +3,18 @@ if [ $(dpkg-query -W -f='${Status}' apache2 2>/dev/null | grep -c "ok installed"
 then
   apt-get install apache2;
 fi
+sleep 3
+if pgrep -x "apache2" >/dev/null
+then echo "1"
+else
+        systemctl start apache2
+fi
+sleep 5
+if apache2ctl -M >/dev/null
+then
+        echo "Apache2 is enabled"
+else
+        systemctl enable apache2
+fi
+sleep 6
+exit
